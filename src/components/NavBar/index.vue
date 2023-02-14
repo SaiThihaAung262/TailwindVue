948e32be5423e2b0c51a96c684a8b0f6e06b7790
minghua <minghua@gmail.com>
2023-02-14 14:00:07 +0630

    Add_Font_awesoe_Icons

--- src/components/NavBar/index.vue
+++ src/components/NavBar/index.vue
@@ -1,14 +1,27 @@
 <template>
-  <div
-    class="navbar flex justify-between items-center bg-midnight w-screen h-10 lg:h-20 md:h-20 sm:h10 pl-10 pr-10"
+  <nav
+    class="navbar flex flex-wrap justify-between items-center w-screen h-10 lg:h-20 md:h-20 sm:h10 pl-10 pr-10"
   >
     <h1
-      class="text-silver text-title-md lg:text-title-lg md:text-title-md sm:text-title-md"
+      class="text-midnight text-title-md font-semibold lg:text-title-lg md:text-title-md sm:text-title-md"
     >
       Website title
     </h1>
-    <div class="text-silver">Here will icons</div>
-  </div>
+    <div class="text-midnight flex justify-between w-[120px]">
+      <font-awesome-icon
+        class="w-7 h-7 cursor-pointer"
+        icon="fa-brands fa-github"
+      />
+      <font-awesome-icon
+        class="w-7 h-7 cursor-pointer"
+        icon="fa-brands fa-facebook"
+      />
+      <font-awesome-icon
+        class="w-7 h-7 cursor-pointer"
+        icon="fa-brands fa-telegram"
+      />
+    </div>
+  </nav>
 </template>
 <script lang="ts" setup></script>
 <style lang="scss" scoped></style>

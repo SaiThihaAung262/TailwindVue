b86d96556afd9f7b8a34481be3ba7964c409f788
minghua <minghua@gmail.com>
2023-02-14 14:45:44 +0630

    Add_colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [
@@ -16,10 +18,32 @@ module.exports = {
       "title-sm": "10px",
     },
     colors: {
+      state: colors.state,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.future,
+      pink: colors.pink,
+      rose: colors.rose,
+      white: colors.white,
+      black: colors.black,
       transparent: "transparent",
       current: "currentColor",
-      white: "#ffffff",
-      purple: "#3f3cbb",
       midnight: "#121063",
       metal: "#565584",
       tahiti: "#3ab7bf",
